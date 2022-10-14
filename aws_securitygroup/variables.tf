264e0ebecb6e66870fa821511398b0c94a77a3a6
Narek <zzz@zzz.zzz>
2022-10-14 16:18:49 +0400

    Added VPC ID var

---
+++ aws_securitygroup/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+
+variable "public_subnet_cidrs" {
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+}
+
+variable "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+
+variable "private_subnet_cidrs" {
+  default = [
+    "10.0.11.0/24",
+    "10.0.22.0/24",
+    "10.0.33.0/24"
+  ]
+}
